b83496f09cfe669f217da62bbee25630d97343c1
Diego Hernandez <dfdiegohernandez95@gmail.com>
2020-06-02 16:36:02 +0200

    uploading html, css and js files

---
+++ js/index.js
@@ -0,0 +1,75 @@
+var map;
+var markers = [];
+var infoWindow;
+
+function initMap() {
+  var berlin = {
+    lat: 52.5243683,
+    lng: 13.4105301,
+  };
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: berlin,
+    zoom: 13,
+  });
+
+  infoWindow = new google.maps.InfoWindow();
+
+  displayStores();
+  showStoresMarkers();
+}
+
+function displayStores() {
+  var storesHtml = "";
+  stores.forEach(function (store, index) {
+    var address = store.addressLines;
+    var phone = store.phoneNumber;
+
+    storesHtml += `<div class="store-container">
+      <div class="store-info-container">
+        <div class="store-address">
+          <span>${address[0]}</span>
+          <span>${address[1]}</span>
+        </div>
+        <div class="store-phone-number">${phone}</div>
+      </div>
+      <div class="store-number-container">
+        <div class="store-number">
+          ${index + 1}
+        </div>
+      </div>
+    </div>`;
+  });
+
+  document.querySelector(".stores-list").innerHTML = storesHtml;
+}
+
+function showStoresMarkers() {
+  var bounds = new google.maps.LatLngBounds();
+  stores.forEach(function (store, index) {
+    var latlng = new google.maps.LatLng(
+      store.coordinates.latitude,
+      store.coordinates.longitude
+    );
+
+    var name = store.name;
+    var address = store.addressLines[0];
+    bounds.extend(latlng);
+    createMarker(latlng, name, address);
+  });
+
+  map.fitBounds(bounds);
+}
+function createMarker(latlng, name, address) {
+  var html = "<b>" + name + "</b><br>" + address;
+  var marker = new google.maps.Marker({
+    map: map,
+    position: latlng,
+  });
+
+  google.maps.event.addListener(marker, "click", function () {
+    infoWindow.setContent(html);
+    infoWindow.open(map, marker);
+  });
+
+  markers.push(marker);
+}
